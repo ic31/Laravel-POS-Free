ad916211461d4508e51cc3c33104efc549db9e38
gedeeinstein <gedeeinstein@gmail.com>
2020-06-25 12:09:02 +0800

    update

--- laravel-pos-tahu-coding.sql
+++ laravel-pos-tahu-coding.sql
@@ -10,7 +10,7 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
-SET time_zone = "+00:00";
+SET time_zone = "+08:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -19,7 +19,6 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `laravel-pos-tahu-coding`
 --
 
 -- --------------------------------------------------------
